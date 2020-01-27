8acd8016ebe3fc333a115eeaa9f4624f0fcbdbba
KalebeLopes <calbkalebe@gmail.com>
2020-01-27 19:39:24 -0300

    grau9

--- Listas/Lista3_verao/4.c
+++ Listas/Lista3_verao/4.c
@@ -1,10 +1,31 @@
 #include <stdio.h>
 
-int multiplo(int n);
+int grau9(int soma){
+    if (soma < 10)
+        return 0;
+
+    else{
+        soma = soma-digitos(soma);
+        return grau9(soma)+1;
+    }
+}
 
 int main(){
 
-    int n;
+    char num[1001];
+    int soma;
+
+    scanf("%s", num);
+
+    for (int i = 0, soma = 0; num[i] != '\0'; i++){
+        soma += num[i] - '0';
+    }
+
+    if (soma % 9 == 0){
+        printf(" %d\n", grau9(soma) + 1);
+    }
+    else
+        printf("%d is not...");
 
     return 0;
 }
