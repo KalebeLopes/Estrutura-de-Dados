d38f3bc5319dcd3c53ceed8bcb8968223afb6e54
KalebeLopes <calbkalebe@gmail.com>
2020-02-05 12:59:39 -0300

    exercicio e Lista 4 -> Divide lista

---
+++ Listas/Lista4_verao/e.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct celula {
+    int dado;
+    struct celula *prox;
+} celula;
+
+void divide_lista (celula *l, celula *l1, celula *l2){
+    
+}
+
+int main(){
+
+    return 0;
+}
