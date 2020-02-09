64d2d3aa0be75e5451e6d4eb37fbb6a95b3e9de8
KalebeLopes <calbkalebe@gmail.com>
2020-02-09 00:26:06 -0300

    Pilha com vetor - desempilha (b)

--- Listas/Lista5_verao/b.c
+++ Listas/Lista5_verao/b.c
@@ -6,72 +6,72 @@ typedef struct pilha {
     int N, topo;
 } pilha;
 
-void inicializa(pilha *p){
-    p->N = 5;
-    p->dados = malloc(p->N * sizeof(int));
+// void inicializa(pilha *p){
+//     p->N = 5;
+//     p->dados = malloc(p->N * sizeof(int));
     
-    if (p->dados == NULL)
-        return;
+//     if (p->dados == NULL)
+//         return;
 
-    p->topo = 0;    
-}
+//     p->topo = 0;    
+// }
 
-int empilha (pilha *p, int x){
-    if (p->dados == NULL)
-        return 0;
+// int empilha (pilha *p, int x){
+//     if (p->dados == NULL)
+//         return 0;
 
-    if (p->topo == p->N){
-        p->dados = realloc(p->dados, 2 * p->N * sizeof(int));
-        if (p->dados == NULL)
-            return 0;
-        p->N *= 2; 
-    }
+//     if (p->topo == p->N){
+//         p->dados = realloc(p->dados, 2 * p->N * sizeof(int));
+//         if (p->dados == NULL)
+//             return 0;
+//         p->N *= 2; 
+//     }
 
-    p->dados[p->topo] = x;
-    p->topo ++;
+//     p->dados[p->topo] = x;
+//     p->topo ++;
 
-    return 1;
-}
+//     return 1;
+// }
 
 int desempilha (pilha *p, int *y){
-    if (p->dados == NULL || p->topo == 0)
+    if (p->dados == NULL || p->topo == NULL)
         return 0;
 
-    int x;
-    
-    p->topo--;
+    else {
+        p->topo--;
 
-    y = &p->dados[p->topo];
-
+        *y = p->dados[p->topo];
+        //printf("val y : %d\n", *y);
+    }
     return 1;
 
 }
 
-int main(){
+// int main(){
 
-    pilha *p;
-    int *y, x;
+//     pilha *p;
+//     int *y;
 
-    p = malloc(sizeof(pilha));
+//     p = malloc(sizeof(pilha));
 
-    inicializa(p);
-    empilha(p, 1);
-    empilha(p, 2);
-    empilha(p, 3);
+//     inicializa(p);
+//     empilha(p, 1);
+//     empilha(p, 2);
+//     empilha(p, 3);
 
-    // for (int i = 0; i < p->topo; i++){
-    //     printf("%d\n", p->dados[i]);
-    // }
+//     // for (int i = 0; i < p->topo; i++){
+//     //     printf("%d\n", p->dados[i]);
+//     // }
 
-    desempilha(p, y);
+//     desempilha(p, y);
 
-    //printf("valor y: %d\n", x);
+//     //printf("valor y: %d\n", *y);
 
-    for (int i = 0; i < p->topo; i++){
-        printf("%d\n", p->dados[i]);
-    }
+//     for (int i = 0; i < p->topo; i++){
+//         printf("%d\n", p->dados[i]);
+//     }
 
     
 
-    return 0;
-}
+//     return 0;
+// }
---
+++ Listas/Lista5_verao/d.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct celula {
+    int dado;
+    struct celula *prox;
+} celula;
+
+int desempilha (celula *p, int *y){
+    
+}
+
+int main(){
+
+    return 0;
+}
